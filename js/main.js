49567f23188e65f0f22e3e2b5163dbe291cc903d
Ahmed Eid <ahmedeidworker@outlook.com>
2024-07-01 00:21:50 +0300

    Finish My Portofolio

--- js/main.js
+++ js/main.js
@@ -34,11 +34,110 @@ const shadowHeader = () => {
 window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+(function () {
+  emailjs.init("2xBeOHhnsfRGeuWHW");
+})();
+
+window.onload = function () {
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_vldmrxx", "template_h6ee82q", contactForm).then(
+      () => {
+        contactMessage.textContent = "Message Send Successfully ✅";
+
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        contactForm.reset();
+      },
+      (error) => {
+        contactMessage.textContent = "Message not sent (service error) ❌";
+        console.error("FAILED...", error);
+      }
+    );
+  });
+};
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUP = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUP);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sctionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav__menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sctionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".home__profile, .about__image, .contact__mail", { origin: "right" });
+sr.reveal(
+  ".home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data",
+  { origin: "left" }
+);
+sr.reveal(".services__card, .projects__card", { interval: 100 });
